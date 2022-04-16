08e2a9767bd6d2da34608706c377cfa56ad99d87
macksonyqli <li.yuanqia@northeastern.edu>
2022-04-16 01:18:59 -0500

    update

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 //const API_BASE = 'http://localhost:4000/api'
 //const REACT_API_BASE = 'https://full-stack-developer-server-cs.herokuapp.com/api'
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
-//const TUITS_API = 'https://full-stack-developer-server-cs.herokuapp.com/api/tuits';
+//const API_BASE = process.env.REACT_APP_API_BASE;
+//const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = 'https://full-stack-developer-server-cs.herokuapp.com/api/tuits';
 //const TUITS_API = 'http://localhost:4000/api/tuits'
 
 
